041f41a36c2f4a8dd3f942562e0b0780ade639aa
gcrobertson <gregcrobertson@gmail.com>
2020-12-22 10:30:05 -0600

    Re-solved Leetcode 0001. Using a hash map two different ways in O(n) and O(n+n) time. Next up is to try to solve 0167 before re-trying 0015 again

--- leetcode/0001/main.go
+++ leetcode/0001/main.go
@@ -1,13 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
+	ex1()
+	// ex2()
+	// ex3()
+	// ex4()
+}
 
+func ex1() {
 	nums := []int{2, 7, 11, 15}
 	sum := 9
+	result := twoSum(nums, sum)
+	fmt.Printf("ex1(): expect: %v\t result: %v\n", []int{0, 1}, result)
+}
 
-	fmt.Printf("%+v", twoSum(nums, sum))
+func ex2() {
+	nums := []int{2, 3, 4}
+	target := 6
+	result := twoSum(nums, target)
+	fmt.Printf("ex2(): expect: %v\t result: %v\n", []int{0, 2}, result)
+}
+
+func ex3() {
+	nums := []int{3, 2, 4}
+	target := 6
+	result := twoSum(nums, target)
+	fmt.Printf("ex3(): expect: %v\t result: %v\n", []int{1, 2}, result)
+}
+
+func ex4() {
+	nums := []int{2, 5, 5, 11}
+	target := 10
+	result := twoSum(nums, target)
+	fmt.Printf("ex4(): expect: %v\t result: %v\n", []int{1, 2}, result)
 }
 
 /*
@@ -38,15 +68,51 @@ func twoSumBruteForce(nums []int, target int) []int {
 		for j := i + 1; j < len(nums); j++ {
 
 			if nums[i]+nums[j] == target {
-				return []int{i, j}
+				return []int{i + 1, j + 1} // non-zero based index result, so add 1 to each index ...
 			}
 		}
 	}
 	return []int{}
 }
 
-// @TODO: Improve the solution above so that I can solve: #0015, # 0167
+func twoSumTwoPass(nums []int, target int) []int {
+
+	hash := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		hash[nums[i]] = i
+	}
+
+	for i := 0; i < len(nums); i++ {
+		search := target - nums[i]
+		if _, ok := hash[search]; ok && hash[search] != i {
+			return []int{i, hash[search]}
+		}
+	}
+
+	return []int{}
+}
+
+// Also O(n) but not O(n+n) as previous ...
 func twoSum(nums []int, target int) []int {
+
+	hash := make(map[int]int) // `value` : `index`
+
+	for i := 0; i < len(nums); i++ {
+
+		search := target - nums[i]
+
+		//fmt.Printf("hash: %v\n", hash)
+		//fmt.Printf("index: %v\t value: %v\t search: %v\n", i, nums[i], search)
+
+		if _, ok := hash[search]; ok {
+			x := []int{i, hash[search]}
+			sort.Ints(x)
+			return x
+		}
+
+		hash[nums[i]] = i // `value` : `index`
+	}
 
 	return []int{}
 }
